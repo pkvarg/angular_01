6c015174b45a3ba696eef349088f9e9e47da2c7f
peter varga <pkvarg@yahoo.se>
2024-08-13 08:12:31 +0200

    first commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input() avatar!: string;
+  @Input() name!: string;
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+
+  onSelectUser() {}
+}
